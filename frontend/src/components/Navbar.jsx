76207ee508009fb21af7c51b19f24ebf0c0bc00e
barakaganai <barakaganai01@gmail.com>
2024-01-17 12:05:45 +0300

    update navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { AuthContext } from "../context/authContext";
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const { currentUser, logout } = useContext(AuthContext);
+
+  const handleSideBarClose = () => {
+    setShow(false);
+  };
 
   return (
     <div className="p-7 flex items-center justify-between">
@@ -31,10 +36,6 @@ const Navbar = () => {
               Register
             </button>
           </Link>
-          <Bars3Icon
-            className="md:hidden h-6 w-6 cursor-pointer"
-            onClick={() => setShow(!show)}
-          />
         </div>
       ) : (
         <div className="flex gap-5 items-center">
@@ -44,25 +45,69 @@ const Navbar = () => {
           </p>
         </div>
       )}
+      {!open ? (
+        <Bars3Icon
+          className="relative md:hidden h-6 w-6 cursor-pointer"
+          onClick={() => {
+            setShow(!show);
+            setOpen(true);
+          }}
+        />
+      ) : (
+        <XMarkIcon
+          className="relative md:hidden h-6 w-6 cursor-pointer"
+          onClick={() => {
+            setShow(!show);
+            setOpen(false);
+          }}
+        />
+      )}
       {show && (
-        <div className="flex flex-col gap-5">
+        <div className="absolute h-screen w-screen top-[100px]  right-0 items-center justify-center bg-white flex flex-col gap-5">
           <Link to="/">
-            <p className="text-xl">Home</p>
+            <p className="text-xl" onClick={handleSideBarClose}>
+              Home
+            </p>
           </Link>
           <Link to="/add">
-            <p className="text-xl">Add</p>
+            <p className="text-xl" onClick={handleSideBarClose}>
+              Add
+            </p>
           </Link>
 
-          <Link to="/login">
-            <button className="text-xl border px-2 py-1 rounded-lg">
-              Login
-            </button>
-          </Link>
-          <Link to="/register">
-            <button className="hidden sm:flex text-xl border px-2 py-1 rounded-lg">
-              Register
-            </button>
-          </Link>
+          {!currentUser ? (
+            <div className="flex flex-col gap-5 items-center">
+              <Link to="/login">
+                <button
+                  className="md:hidden text-xl border px-2 py-1 rounded-lg"
+                  onClick={handleSideBarClose}
+                >
+                  Login
+                </button>
+              </Link>
+              <Link to="/register">
+                <button
+                  className="md:hidden text-xl border px-2 py-1 rounded-lg"
+                  onClick={handleSideBarClose}
+                >
+                  Register
+                </button>
+              </Link>
+            </div>
+          ) : (
+            <div className="flex flex-col gap-5 items-center">
+              <p className="text-lg font-semibold">{currentUser.username}</p>
+              <p
+                className="text-xl cursor-pointer font-bold"
+                onClick={() => {
+                  logout();
+                  handleSideBarClose();
+                }}
+              >
+                Logout
+              </p>
+            </div>
+          )}
         </div>
       )}
     </div>
